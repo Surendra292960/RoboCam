ccf94b8e20d8e6482ae4353ccc7a377b9f5e909b
Surendra292960 <surendrapalatm@gmail.com>
2024-10-08 13:54:01 +0530

    Initial commit

--- app/src/main/java/com/example/robocam/joystick/JoyStickController.kt
+++ app/src/main/java/com/example/robocam/joystick/JoyStickController.kt
@@ -1,17 +1,28 @@
 package com.example.robocam.joystick
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ColorMatrix
 import android.util.Log
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,12 +30,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.robocam.R
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -37,6 +59,11 @@ fun JoyStickController(onCoordinatesChange: (String) -> Unit) {
     val joystickRadius = 70f // Radius of the joystick itself
     val center = Offset(150f, 150f)
     val haptic = LocalHapticFeedback.current
+    val context = LocalContext.current
+
+    // Creating animation
+    val animateFloat = remember { Animatable(0f) }
+    val startAnim = remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -65,6 +92,7 @@ fun JoyStickController(onCoordinatesChange: (String) -> Unit) {
                         joystickOffset = newOffset
                     } else {
                         Log.d("TAG", "JoyStickController: distance greater $distance")
+                        startAnim.value = true
                         haptic.performHapticFeedback(HapticFeedbackType.LongPress)
 
                         // Scale the newOffset to the circle's boundary minus joystick radius
@@ -84,6 +112,14 @@ fun JoyStickController(onCoordinatesChange: (String) -> Unit) {
                 }
             }
     ) {
+        if (startAnim.value){
+            LaunchedEffect(key1 = startAnim.value) {
+                animateFloat.animateTo(
+                    targetValue = 1f,
+                    animationSpec = tween(durationMillis = 1, easing = LinearEasing))
+            }
+        }
+
         // Draw the circle and joystick
         Canvas(modifier = Modifier
             .fillMaxSize()
@@ -93,8 +129,12 @@ fun JoyStickController(onCoordinatesChange: (String) -> Unit) {
                 height = 120.dp,
             )) {
             drawCircle(color = Color.LightGray, radius = circleRadius, center = center)
-            drawCircle(color = Color.Gray, radius = joystickRadius, center = joystickOffset)
+
+            drawCircle(color = Color.Gray, radius = joystickRadius, center = joystickOffset).apply {
+                ImageVector
+            }
         }
+
 
         // Display coordinates
         Text(
